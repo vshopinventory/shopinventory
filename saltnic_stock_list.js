ab84c56be14d863d42182d4a8abac15378395bea
vshopinventory <magicmart07@gmail.com>
2025-02-16 13:08:36 +0600

    Update saltnic_stock_list.js
    Kings Crest Tabaco Dulce 35mg

--- saltnic_stock_list.js
+++ saltnic_stock_list.js
@@ -1,6 +1,6 @@
 //Item Lists
 const items={
-    kc_tabaco_dulce_35mg:{quantity:17, reserved:"N"},
+    kc_tabaco_dulce_35mg:{quantity:16, reserved:"N"},
     kc_tabaco_dulce_50mg:{quantity:2, reserved:"N"},
     kc_bombo_aldonza_50mg:{quantity:13, reserved:"N"},
     kc_bombo_aldonza_35mg:{quantity:10, reserved:"N"},
@@ -341,10 +341,10 @@ const items={
     document.getElementById('sum').innerHTML=sum;
 
     //Update Status
-    let date="14/02/2025";
-    let time="4:00 PM";
+    let date="16/02/2025";
+    let time="1:07 PM";
     let updatedProducts=`<strong>Last updated Product(s):</strong>
- Skwezed Green Apple 25mg Iced`;
+ Kings Crest Tabaco Dulce 35mg`;
     document.getElementById("date").innerHTML=date;
     document.getElementById("time").innerHTML=time;
     document.getElementById('updatedProducts').innerHTML=updatedProducts;
